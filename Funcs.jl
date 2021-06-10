6629a1257fa521f454a113e7eaad17db59ba11da
mramalingam2203 <vaanimuthu@gmail.com>
2021-06-10 11:02:48 +0530

    unit tested two-ode

--- Funcs.jl
+++ Funcs.jl
@@ -20,8 +20,12 @@ function parameters(k, ksi, k_g, Kappa_g)
     yl = rand([0.0, 1.0], 4, 5);
     yr = rand([0.0, 1.0], 4, 5);
     p = rand([0.0, 1.0], 1, 5);
-
     twobc(yl, yr, p, paramsH, paramsL)
+
+    c = 0.01929;
+    region = 1
+    f= rand(Float64, (1, 4))
+    twoode(c, f, region, paramsH, paramsL)
     return [paramsL paramsH]
 end
 
@@ -41,7 +45,6 @@ end
 
 
 function twobc(yl, yr, p, paramsH, paramsL)
-
     # Boundary conditions
     i1 = (1 / paramsH.theta) * (yr[1,4] - p[4] - 1) + paramsH.v;
     g_i1 = 0.5 * paramsH.theta * (i1 - paramsH.v)^2;
@@ -83,11 +86,33 @@ function twobc(yl, yr, p, paramsH, paramsL)
 
             paramsH.r * yr[1,4]- (i1 - paramsH.delta + paramsH.mu)*(yr[1,4] - p[4])-((paramsH.r - paramsH.lambda) * p[4] +paramsH.alpha - i1 - g_i1)- paramsH.ksi *(yr[3,4] - yr[1,4]),
             paramsL.r * yr[3,5] - (i2 - paramsL.delta + paramsL.mu) *(yr[3,5] - p[5])- ((paramsL.r - paramsL.lambda) * p[5] +paramsL.alpha - i2 - g_i2) - paramsL.ksi * (yr[1,5] - yr[3,5])
-
             ];
+end
 
-        print(res)
 
+function twoode(c, f, region, paramsH, paramsL)
+    # ODE equations
+    a13 = 0.5 * (paramsH.sigmaP^2 * c^2 - 2 * paramsH.sigmaP *paramsH.sigmaA * paramsH.rho * c + paramsH.sigmaA^2)
+    a23 = 0.5 * (paramsL.sigmaP^2 * c^2 - 2 * paramsL.sigmaP *paramsL.sigmaA * paramsL.rho * c + paramsL.sigmaA^2)
+    i1 = (1 / paramsH.theta) * ((f[1] / f[2]) - c - 1) + paramsH.v
+    g_i1 = 0.5 * paramsH.theta * (i1 - paramsH.v)^2
+
+    i2 = (1 / paramsL.theta) * ((f[3] / f[4]) - c - 1) + paramsL.v
+    g_i2 = 0.5 * paramsL.theta * (i2 - paramsL.v)^2
+
+    a2 = (paramsH.r * f[1] - ((paramsH.r - paramsH.lambda) * c +
+        paramsH.alpha - i1 - g_i1) * f[2] - (i1 - paramsH.delta
+        + paramsH.mu) * (f[1] - c * f[2]) - paramsH.ksi *
+        (f[3] - f[1])) / a13
+
+    a4 = (paramsL.r * f[3] - ((paramsL.r - paramsL.lambda) * c +
+        paramsL.alpha - i2 - g_i2) * f[4] - (i2 - paramsL.delta
+        + paramsL.mu) * (f[3] - c * f[4]) - paramsL.ksi *
+        (f[1] - f[3])) / a23
+
+    dfdc = [f[2] a2 f[4] a4]
+
+    return dfdc
 end
 
 
@@ -150,86 +175,4 @@ function f_solver(parmets, p)
 end
 
 
-function twobc(yl, yr, p, paramsH, paramsL)
-    # Boundary conditions
-
-    i1 = (1 / paramsH.theta) * (yr[1 4] - p[4] - 1) + paramsH.v;
-    g_i1 = 0.5 * paramsH.theta * (i1 - paramsH.v)^2;
-
-
-    i2 = (1 / paramsL.theta) * (yr[3 5] - p[5] - 1) + paramsL.v;
-    g_i2 = 0.5 * paramsL.theta * (i2 - paramsL.v)^2;
-
-    res = [
-            yl[3 1] - yr[3 3] + paramsL.phi +
-            (1 + paramsL.gamma) * p[3]
-
-            yl[1 2] - yr[1 2] + paramsH.phi +
-            (1 + paramsH.gamma) * (p[2] - p[1])
-
-            yl[1 2] - yr[1 1]
-            yl[2 2] - yr[2 1]
-            yl[3 2] - yr[3 1]
-            yl[4 2] - yr[4 1]
-
-            # Conditions at m_H
-            yl[1 3] - yr[1 2]
-            yl[2 3] - yr[2 2]
-            yl[3 3] - yr[3 2]
-            yl[4 3] - yr[4 2]
-
-            # Conditions at m_L
-            yl[1 4] - yr[1 3]
-            yl[2 4] - yr[2 3]
-            yl[3 4] - yr[3 3]
-            yl[4 4] - yr[4 3]
-
-            # Conditions at upper boundary 1
-            yl[2 5] - yr[2 4]
-            yl[3 5] - yr[3 4]
-            yl[4 5] - yr[4 4]
-
-            # Conditions at upper boundary 2
-            yr[1 5] - yl[1 5] - p[5] + p[4];
-
-            paramsH.r * yr[1 4] - (i1 - paramsH.delta + paramsH.mu) *(yr[1, 4] - p(4)) - ((paramsH.r - paramsH.lambda) * p[4] +
-            paramsH.alpha - i1 - g_i1) - paramsH.ksi *(yr[3 4] - yr[1 4])
-
-
-            paramsL.r * yr[3 5] - (i2 - paramsL.delta + paramsL.mu) *(yr[3 5] - p[5])- ((paramsL.r - paramsL.lambda) * p[5] +
-            paramsL.alpha - i2 - g_i2) - paramsL.ksi * (yr[1 5] - yr[3 5])
-        ]
-
-end
-
-
-
-function dfdc = twoode(c, f, region, paramsH, paramsL)
-    # ODE equations
-    a13 = 0.5 * (paramsH.sigmaP^2 * c^2 - 2 * paramsH.sigmaP *paramsH.sigmaA * paramsH.rho * c + paramsH.sigmaA^2)
-
-    a23 = 0.5 * (paramsL.sigmaP^2 * c^2 - 2 * paramsL.sigmaP *paramsL.sigmaA * paramsL.rho * c + paramsL.sigmaA^2)
-
-    i1 = (1 / paramsH.theta) * ((f[1] / f[2]) - c - 1) + paramsH.v
-    g_i1 = 0.5 * paramsH.theta * (i1 - paramsH.v)^2
-
-
-    i2 = (1 / paramsL.theta) * ((f[3] / f[4]) - c - 1) + paramsL.v
-    g_i2 = 0.5 * paramsL.theta * (i2 - paramsL.v)^2
-
-
-    a2 = (paramsH.r * f[1] - ((paramsH.r - paramsH.lambda) * c +
-        paramsH.alpha - i1 - g_i1) * f[2] - (i1 - paramsH.delta
-        + paramsH.mu) * (f[1] - c * f[2]) - paramsH.ksi *
-        (f[3] - f[1])) / a13
-
-    a4 = (paramsL.r * f[3] - ((paramsL.r - paramsL.lambda) * c +
-        paramsL.alpha - i2 - g_i2) * f[4] - (i2 - paramsL.delta
-        + paramsL.mu) * (f[3] - c * f[4]) - paramsL.ksi *
-        (f[1] - f[3])) / a23
-
-    #dfdc = [f[2] a2 f[4] a4]
-
-    #return dfdc
-end
 =#
